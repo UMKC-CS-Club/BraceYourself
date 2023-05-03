2c68619edb3751bc37bedac15fd40ae21aec14bf
creallfluharty <creallfluharty@gmail.com>
2023-05-03 15:49:23 -0500

    add support for new canvas

--- terminal_interface.py
+++ terminal_interface.py
@@ -1,6 +1,52 @@
 import json
+from dataclasses import dataclass
+from typing import Any, Callable, Union, Optional
 
 from KnotContainer import KnotContainer
+
+
+@dataclass
+class Success:
+    value: Any
+
+
+@dataclass
+class Failure:
+    message: str
+
+
+Result = Union[Success, Failure]
+Numeric = Union[int, float]
+
+
+def prompt_user(prompt: str, validator_extractor: Callable[[str], Result]):
+    while True:
+        inp = input(prompt)
+        result = validator_extractor(inp)
+        if isinstance(result, Success):
+            return result.value
+
+        print(result.message)
+
+
+def make_inclusive_range_validator(lower: Numeric, upper: Numeric) -> Callable[[Numeric], bool]:
+    def range_validator(num):
+        return lower <= num <= upper
+
+    return range_validator
+
+
+def prompt_number(prompt, lower, upper):
+    validator = make_inclusive_range_validator(lower, upper)
+
+    def validator_extractor(inp):
+        num = int(inp)
+        if validator(num):
+            return Success(num)
+
+        return Failure(f"Number is not in range [{lower}, {upper}]")
+
+    return prompt_user(prompt, validator_extractor)
 
 
 def prompt_options(prompt, options):
@@ -18,39 +64,33 @@ def prompt_options(prompt, options):
             print(f"{choice} doesn't appear to be between 1 and {len(options)}. Please try again.")
 
 
-def prompt_load_file(prompt, loader):
-    print(prompt)
-
-    while True:
-        f_name = input("> ")
+def prompt_load_file(prompt, extractor_validator):
+    def _extractor_validator(f_name):
         try:
             f = open(f_name)
         except FileNotFoundError:
-            print(f"Couldn't find a file at {f_name}. Please try another path.")
-            continue
+            return Failure(f"Couldn't find a file at {f_name}. Please try another path.")
 
         with f:
-            loaded, ret = loader(f)
-            if loaded:
-                return ret
+            return extractor_validator(f)
 
-            print(f"Couldn't load from the file at {f_name}. Please try another file.")
+    return prompt_user(prompt, _extractor_validator)
 
 
 def prompt_user_canvas():
-    n_strings = 12
-    n_primary_rows = 12
+    n_strings = prompt_number("How many strings? ", 2, float("inf"))
+    n_primary_rows = prompt_number("How many rows? ", 2, float("inf"))
     return KnotContainer.empty(n_strings, n_primary_rows)
 
 
 def prompt_user_save():
-    def loader(file):
+    def loader(f_name):
         try:
-            return True, KnotContainer.from_dict(json.load(file))
+            return Success(KnotContainer.from_dict(json.load(f_name)))
         except (json.JSONDecodeError, KeyError, TypeError) as e:
-            return False, None
+            return Failure(f"Couldn't load from the file at {f_name}. Please try another file.")
 
-    return prompt_load_file("Enter the path of the file to load:", loader)
+    return prompt_load_file("Enter the path of the file to load: ", loader)
 
 
 def get_knot_container():
